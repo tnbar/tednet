fd433430391cb60dc99eb3f402e2d09f98b4223b
perry <perry@dr.space>
2020-12-06 15:20:27 +0800

    init

--- tests/test_tr.py
+++ tests/test_tr.py
@@ -1,3 +1,18 @@
 # -*- coding: UTF-8 -*-
 
+import sys
+sys.path.append('../tednet')
 
+import torch
+from tednet.tnn.tensor_ring import TRLeNet5
+
+
+class Test_TR(object):
+    def test_bmnist(self):
+        model = TRLeNet5(10, rs=[6, 6, 6, 6])
+
+        data = torch.Tensor(16, 1, 28, 28)
+
+        res = model(data)
+
+        assert len(res.shape) == 2
